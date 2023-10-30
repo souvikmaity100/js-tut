9513a818e866b63e88e876626b8b17f3550a3ee5
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-10-30 16:37:44 +0000

    datatypes in js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser thats why we dont use this syntex 
+
+console.log(3 
+    +
+     3) // code readability drop's 
+
+console.log("souvik")
+
+
+let name = "souvik"
+let age = 31
+let isLoggedIn = false
+let state; // just define a veriable not assign any value
+
+// number => 2 to power 53
+// bigint => use for big numbers
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
