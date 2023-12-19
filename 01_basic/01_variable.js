8c088086ba8887536d2c79272e15eb2775a0bb72
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-12-19 15:34:07 +0000

    more on varible

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -20,3 +20,30 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+/* ---------------- more on varible ----------------*/
+
+var a = 67 // a contains 67
+        console.log(a);
+        a = "Souvik maity";
+        console.log(a);
+        // let 8ossam = 7 // Not allowed this will throw an error
+        // let var = 7
+
+
+        var a = 45;
+        // var a = "p"
+        let b = "souvik";
+        const author = "suman"
+        // author = 50 // Throws an error because constant cannot be changed
+        b = 14
+        const sm = 0;
+        let c = null
+        let d  = undefined  
+        {
+        let b = 'this is a string'
+        console.log(b)
+        }
+        console.log(b)
