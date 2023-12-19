e29046dd6806b42dafaae43fe28c2f80475f866a
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-12-19 15:37:30 +0000

    js operators

---
+++ 01_basic/04_operators.js
@@ -0,0 +1,77 @@
+// 4. -------Operatots in JavaScript-------
+    // 4.1- Arathmatic Operators
+    var number1 = 45;
+    var number2 = 12;
+    // console.log("The value of number1 + numbaer2 is ", number1 + number2);
+    // console.log("The value of number1 - numbaer2 is ", number1 - number2);
+    // console.log("The value of number1 * numbaer2 is ", number1 * number2);
+    // console.log("The value of number1 / numbaer2 is ", number1 / number2);
+
+    // 4.2- Assignment Operatots
+    var a2 = 33;
+    var b2 = 10;
+    // result = (a2 += b2);
+    // console.log(result);
+    // result = (a2 -= b2);
+    // console.log(result);
+    // result = (a2 *= b2);
+    // console.log(result);
+    // result = (a2 /= b2);
+    // console.log(result);
+    // result = (a2 %= b2);
+    // console.log(result);
+
+    // 4.3- Comparison Operators
+    var x = 26;
+    var y = 45;
+    // console.log(x == y);
+    // console.log(x != y);
+    // console.log(x > y);
+    // console.log(x < y);
+    // console.log(x <= y);
+    // console.log(x >= y);
+
+    // 4.4- Logical Operators
+    // logical and
+    // console.log(true && true);
+    // console.log(true && false);
+    // console.log(false && false);
+
+    // logical or
+    // console.log(true || true);
+    // console.log(true || false);
+    // console.log(false || false);
+
+    // logical not
+    // console.log(!false);
+    // console.log(!true);
+
+    // 4.5- Bitwise Operators
+    var a1 = 7;
+    var b1 = 2;
+    // console.log(a1 & b1);
+    // console.log(a1 | b1);
+    // console.log(a1 ^ b1);
+    // console.log(~b1);
+    console.log(a1 >> b1);
+    console.log(a1 << b1);
+
+    // 4.6- Miscellaneous Operators
+    var a3 = 10;
+    var b3 = 20;
+    // alternative of if else
+    // result = (a3 > b3) ? 100 : 200;
+    // console.log(result);
+    // result = (a3 < b3) ? 100 : 200;
+    // console.log(result);
+    a=10; b=10;
+    a++; b--;
+
+    // 4.7- typeof Operator
+    var a4 = 12;
+    var b4 = "abcd";
+    console.log(typeof b4);
+    result = (typeof a4 == "string" ? "a4 is string" : "a4 is numaric");
+    console.log(result);
+    // result = (typeof b4 == "string" ? "b4 is string" : "b4 is numaric");
+    // console.log(result);
