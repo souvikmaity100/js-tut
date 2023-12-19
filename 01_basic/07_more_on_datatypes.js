752530321f1b207ff9d2d242c9e4b8d019eca23b
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-12-19 16:33:53 +0000

    Stack and Heap memory in JavaScript

--- 01_basic/07_more_on_datatypes.js
+++ 01_basic/07_more_on_datatypes.js
@@ -35,3 +35,33 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// -----------------------------------------------------------------------
+
+// Stack and Heap memory in JavaScript
+
+// stack used in primitive datatypes | Heap used in non-primitive datatype
+
+let myName = "sam roy";
+
+let anotherName = myName;
+
+anotherName = "ram";
+
+console.log(myName);
+console.log(anotherName); 
+
+
+let user1 = {
+    name: 'sam',
+    role: 'web developer'
+};
+
+let user2 = user1;
+
+user2.role = 'programmer';
+
+console.log(user1);
+console.log(user2);
