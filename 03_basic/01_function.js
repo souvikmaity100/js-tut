bdcdecc89f20aa51e77c7da1e6cca8b3dc520c9b
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-12-25 17:13:00 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,72 @@
+
+function sayMyName(){
+    console.log("SOUVIK");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+function loginUserMessage(username){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("ram"))
+// console.log(loginUserMessage())
+
+// -----------rest operator: The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, ex: (...num1) -----------------
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "rohan",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
