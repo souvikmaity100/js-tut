36c769293e29e687d66db5c9a9be3e9b1ff7a30f
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2023-12-26 16:44:40 +0000

    switch case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+let grade = "B";
+// switch (grade) {
+//     case "A":
+//         console.log("Good Job");
+//         break;
+//     case "B":
+//         console.log("Pretty good");
+//         break;
+//     case "C":
+//         console.log("Passed");
+//         break;
+//     case "D":
+//         console.log("Not so good");
+//         break;
+//     case "F":
+//         console.log("Failed");
+//         break;
+
+//     default:
+//         console.log("Unknown grade");
+//         break;
+// }
