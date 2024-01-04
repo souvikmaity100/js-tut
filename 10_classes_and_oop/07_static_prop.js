d066e99448c8a655c04105dc9e5f6b0344aca868
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-01-04 17:21:35 +0000

    static property in js

---
+++ 10_classes_and_oop/07_static_prop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const sam = new User("sam")
+// console.log(sam.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// console.log(iphone.createId());
