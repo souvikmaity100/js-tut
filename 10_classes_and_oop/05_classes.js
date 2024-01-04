d87ce9dd0d8f4353d88ee007a84e371cff061b66
Souvik Maity <87945067+souvikmaity100@users.noreply.github.com>
2024-01-04 17:02:26 +0000

    class in js

---
+++ 10_classes_and_oop/05_classes.js
@@ -0,0 +1,43 @@
+// ES6
+
+class User {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const sam = new User("sam", "sam@gmail.com", "123")
+
+console.log(sam.encryptPassword());
+console.log(sam.changeUsername());
+
+// behind the scene
+
+function User2(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User2.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User2.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User2("joy", "joy@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
